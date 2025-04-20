6624d6e7b3d0014d56d08db6ee9f77aa18b16328
gcpraghav301 <gcpraghav301@gmail.com>
2025-04-20 11:06:04 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,8 +1,21 @@
-class test
-{ 
-public static void main(String[] args)
- 
-{ 
-System.out.println("Hello World");
-}
+public class HelloWorld {
+
+    // Main method - program execution starts here
+    public static void main(String[] args) {
+
+        // Print a welcome message
+        System.out.println("***************************");
+        System.out.println("*                         *");
+        System.out.println("*     Hello, world!       *");
+        System.out.println("* Welcome to Java Basics! *");
+        System.out.println("*                         *");
+        System.out.println("***************************");
+
+        // A blank line for spacing
+        System.out.println();
+
+        // Another basic message
+        System.out.println("This is your first Java program.");
+        System.out.println("Happy coding!");
+    }
 }
